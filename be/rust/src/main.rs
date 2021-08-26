fb19da5e6381552e08006cf4a39c54e17a79880d
Adam Berg <adam@xyzdigital.com>
2021-08-25 22:06:31 -0700

    Rust: fetch rooms via rust

--- be/rust/src/main.rs
+++ be/rust/src/main.rs
@@ -1,12 +1,42 @@
-#![feature(proc_macro_hygiene, decl_macro)]
+use mongodb::{Client, options::ClientOptions};
+use futures::stream::TryStreamExt;
+use mongodb::{bson::doc, options::FindOptions};
+use serde::{Deserialize, Serialize};
+use mongodb::bson::oid::ObjectId;
+
 
 #[macro_use] extern crate rocket;
 
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+#[derive(Clone, Serialize, Deserialize, PartialEq, Debug)]
+struct Room {
+    #[serde(rename = "_id")]
+    id: ObjectId,
+    title: String
 }
 
-fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+
+#[get("/")]
+async fn rooms() -> Option<String> {
+    let mut client_options = ClientOptions::parse("mongodb://localhost:27017").await.ok()?;
+    let client = Client::with_options(client_options).ok()?;
+    let db = client.database("chat");
+    let typed_collection = db.collection::<Room>("rooms");
+
+    let filter = doc! { };
+    let find_options = FindOptions::builder().build();
+    let mut cursor = typed_collection.find(filter, find_options).await.ok()?;
+
+    println!("rooms ");
+
+    // Iterate over the results of the cursor.
+    while let Some(book) = cursor.try_next().await.ok()? {
+        println!("oid: {}", book.id);
+        println!("title: {}", book.title);
+    }
+    return Some("Hello, world!".to_string())
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/api/rooms", routes![rooms])
 }
